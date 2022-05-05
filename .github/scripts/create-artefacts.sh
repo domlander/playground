0f3000e6f17adc09a3e7729fa3d5eddce41454e6
Dominik Lander <lander45@hotmail.co.uk>
2022-05-05 15:31:11 +0100

    fix paths to src

--- .github/scripts/create-artefacts.sh
+++ .github/scripts/create-artefacts.sh
@@ -6,7 +6,7 @@ cd builds
 echo "Creating TCFV2 artfact..."
 mkdir -p tcfv2/nodejs/node_modules
 cd tcfv2
-cp ../src/cmp_tcfv2.js tcfv2/nodejs/node_modules/pageLoadBlueprint.js
+cp ../../src/cmp_tcfv2.js tcfv2/nodejs/node_modules/pageLoadBlueprint.js
 zip -r nodejs.zip nodejs
 rm -rf nodejs
 cd ..
@@ -15,7 +15,7 @@ echo "TCFV2 artifact created"
 echo "Creating CCPA artifact..."
 mkdir -p ccpa/nodejs/node_modules
 cd ccpa
-cp ../src/cmp_ccpa.js ccpa/nodejs/node_modules/pageLoadBlueprint.js
+cp ../../src/cmp_ccpa.js ccpa/nodejs/node_modules/pageLoadBlueprint.js
 zip -r nodejs.zip nodejs
 rm -rf nodejs
 cd ..
@@ -24,7 +24,7 @@ echo "CCPA artifact created"
 echo "Creating AUS artifact..."
 mkdir -p aus/nodejs/node_modules
 cd aus
-cp ../src/cmp_aus.js aus/nodejs/node_modules/pageLoadBlueprint.js
+cp ../../src/cmp_aus.js aus/nodejs/node_modules/pageLoadBlueprint.js
 zip -r nodejs.zip nodejs
 rm -rf nodejs
 cd ..
